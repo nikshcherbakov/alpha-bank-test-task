b1d7bb7985bec30e5006fa0aaeee2d57612f5eda
Nikita Shcherbakov <shcerbackow@yandex.ru>
2021-09-03 17:08:25 +0300

    Initial commit

---
+++ src/main/java/com/nikshcherbakov/alphabanktesttask/AlphaBankTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.nikshcherbakov.alphabanktesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaBankTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaBankTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikshcherbakov/alphabanktesttask/AlphaBankTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikshcherbakov.alphabanktesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaBankTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
